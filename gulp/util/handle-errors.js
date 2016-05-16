90858f1cbd1f6fef4cf370e3c43a2991e0e46727
Wade <wadeanthony0100@gmail.com>
2016-05-16 14:45:53 -0400

    Fix the build system

--- gulp/util/handle-errors.js
+++ gulp/util/handle-errors.js
@@ -1,12 +1,14 @@
+'use strict';
+
 import notify from 'gulp-notify';
 
 export default function handleErrors(...args) {
 
   // Send error to notification center with gulp-notify
-  notify.onError({
+  Reflect.apply(notify.onError({
     title: 'Compile Error',
     message: '<%= error.plugin %>: <%= error.message %>',
-  })(...args);
+  }), null, args);
 
   // Keep gulp from hanging on this task
   this.emit('end'); // eslint-disable-line no-invalid-this
--- src/js/app.jsx
+++ src/js/app.jsx
@@ -4,7 +4,7 @@ import Header from './components/header';
 
 window.onload = () =>  {
 	ReactDOM.render(
-    <header></header>
+    <Header username='wad'></Header>
     ,document.getElementById('app')
 	);
 };
